16d6836d82779112e661639da2e5b8918369fceb
nebisin <massive00@windowslive.com>
2021-11-06 20:59:23 +0300

    add redis

--- consumer.js
+++ consumer.js
@@ -1,11 +1,13 @@
 const amqp = require("amqplib");
 const data = require("./data.json");
+const redis = require("redis");
+const client = redis.createClient();
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
 
 const queueName = process.argv[2] || "jobsQueue";
-
-const message = {
-  description: "this is a test message",
-};
 
 connect_rabbitmq();
 
@@ -13,7 +15,7 @@ async function connect_rabbitmq() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
-    const assertion = await channel.assertQueue(queueName);
+    await channel.assertQueue(queueName);
 
     // accept the message
     console.log("waiting the messages");
@@ -21,8 +23,18 @@ async function connect_rabbitmq() {
       const messageInfo = JSON.parse(msg.content.toString());
       const userInfo = data.find((u) => (u.id = messageInfo.description));
       if (userInfo) {
-        console.log("User:", userInfo);
-        channel.ack(msg);
+        client.set(
+          `user_${userInfo.id}`,
+          JSON.stringify(userInfo),
+          (err, reply) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log(reply);
+              channel.ack(msg);
+            }
+          }
+        );
       }
     });
   } catch (error) {
--- publisher.js
+++ publisher.js
@@ -13,7 +13,7 @@ async function connect_rabbitmq() {
   try {
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
-    const assertion = await channel.assertQueue(queueName);
+    await channel.assertQueue(queueName);
 
     data.forEach((i) => {
       message.description = i.id;
